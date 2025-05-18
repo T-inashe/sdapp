608f0f04e2cffd4fa5ab3f1034c37122837b4a5f
T-inashe <2588966@students.wits.ac.za>
2025-05-18 23:52:35 +0200

    lets deploy

--- BackEnd/Authentication/RouteA.js
+++ BackEnd/Authentication/RouteA.js
@@ -6,7 +6,7 @@ import User from '../Api/Models/User.js';
 const router = express.Router();
 
 // Get frontend URL from environment variable or use default for local development
-const FRONTEND_URL = 'http://localhost:5173';
+const FRONTEND_URL = 'https://icy-desert-06b45041e.6.azurestaticapps.net/';
 
 // Google OAuth login route
 router.get(
